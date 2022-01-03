814e2649376d8db7ca1138bf22a846bde7d541f3
joaostefani <joaomstefani@gmail.com>
2022-01-03 18:39:33 -0300

    Configuracoes do Sistema de custormer

---
+++ src/main/java/edu/unifacef/customersapi/configurations/mongodb/MongoDbConfiguration.java
@@ -0,0 +1,25 @@
+package edu.unifacef.customersapi.configurations.mongodb;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+@EnableMongoAuditing
+@EnableMongoRepositories(basePackages = {"edu.unifacef.customersapi.gateways.outputs.mongodb"})
+public class MongoDbConfiguration {
+
+    @Autowired
+    private MappingMongoConverter mappingMongoConverter;
+
+    @PostConstruct
+    public void setUpMongoEscapeCharacterConversion(){
+        mappingMongoConverter.setTypeMapper(new DefaultMongoTypeMapper(null));
+    }
+
+}
